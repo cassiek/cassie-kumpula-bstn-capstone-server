1ded2618193b6f0ed4fd6de30aa4b784291059d0
cassiek <ckumpula123@gmail.com>
2024-10-24 20:10:24 -0400

    finish adding data to inventory-ii seed file

--- seeds/inventory-ii.js
+++ seeds/inventory-ii.js
@@ -10,140 +10,140 @@ exports.seed = async function(knex) {
       id: 1, 
       name: "Long Tie Belt Coat",
       variants: '["charcoal", "black", "silver", "grey plaid"]',
-      description: "",
+      description: "Long coat in woven fabric with tie belt and vent at back. Lined.",
       category: "Outerwear"
     },
     {
       id: 2, 
       name: "Crochet Knit Sweater",
       variants: '["gold", "burgundy", "plum", "indigo"]',
-      description: "",
+      description: "Soft, crochet knit sweater with wool. Round neckline.",
       category: "Sweaters"
     },
     {
       id: 3, 
       name: "Long Puffer Jacket",
       variants: '["black", "graphite", "white", "pink", "plum"]',
-      description: "",
+      description: "Long jacket with padding. Concealed, insulating cuffs.",
       category: "Outerwear"
     },
     {
       id: 4, 
       name: "Mock Turtleneck Dress",
       variants: '["black", "sage", "olive", "charcoal"]',
-      description: "",
+      description: "Knee length, fitted knit dress.",
       category: "Dresses"
     },
     {
       id: 5, 
       name: "Sweatshirt",
       variants: '["tan", "navy", "warm white", "sage", "beige"]',
-      description: "",
+      description: "Roomy sweatshirt with soft inside.",
       category: "Sweatshirts"
     },
     {
       id: 6, 
       name: "Straight Leg Pants",
       variants: '["black", "graphite", "khaki", "grey plaid"]',
-      description: "",
+      description: "Long leg pants in woven fabric. Regular waist. Concealed zip fly.",
       category: "Pants"
     },
     {
       id: 7, 
       name: "High Waisted Jeans",
       variants: '["dark", "medium", "light"]',
-      description: "",
+      description: "Slim fit, five pocket, high-waisted jeans in cotton denim. Typically falls to top of foot.",
       category: "Jeans"
     },
     {
       id: 8, 
       name: "Wide Leg Pants",
       variants: '["charcoal", "black", "white"]',
-      description: "",
+      description: "Loose fit, wide leg pants with pleats.",
       category: "Pants"
     },
     {
       id: 9, 
       name: "Fine Knit Sweater",
       variants: '["brown", "burgundy", "light grey"]',
-      description: "",
+      description: "Soft fine knit sweater. Ribbing at neckline, cuffs, and hem.",
       category: "Sweaters"
     },
     {
       id: 10, 
       name: "Lightweight Cotton Shirt",
       variants: '["white", "warm white", "silver", "light grey"]',
-      description: "",
+      description: "Shirt in woven fabric.",
       category: "Shirts"
     },
     {
       id: 11, 
       name: "Chiffon Blouse",
       variants: '["white", "lilac", "light gold"]',
-      description: "",
+      description: "Long-sleeved, loose-fitting blouse in solid chiffon.",
       category: "Blouses"
     },
     {
       id: 12, 
       name: "Oxford Shirt",
       variants: '["white", "light blue", "light grey"]',
-      description: "",
+      description: "Washed oxford cotton shirt.",
       category: "Shirts"
     },
     {
       id: 13, 
       name: "Feathersoft Blouse",
       variants: '["black", "warm white", "grey"]',
-      description: "",
+      description: "Loose fitting blouse in soft twill. Ruffled collar.",
       category: "Blouses"
     },
     {
       id: 14, 
       name: "Long Sleeve Top",
       variants: '["white", "warm white", "sage", "light gold"]',
-      description: "",
+      description: "Long, fitted top in soft jersey. No ribbing.",
       category: "Tops"
     },
     {
       id: 15, 
       name: "Crew Neck Long Top",
       variants: '["charcoal", "navy", "plum", "light blue"]',
-      description: "",
+      description: "Long, fitted top in soft jersey. Long sleeves and straight-cut hem.",
       category: "Tops"
     },
     {
       id: 16, 
       name: "High Waisted Soft Jeans",
       variants: '["black", "graphite", "dark", "light"]',
-      description: "",
+      description: "Skinny fit, gently stretching high-waisted jeans. Extra soft cotton denim.",
       category: "Jeans"
     },
     {
       id: 17, 
       name: "Slacks",
       variants: '["charcoal", "black", "khaki"]',
-      description: "",
+      description: "Slacks with regular waist, concealed elasticized waistband, and zip with concealed hook and eye fastener.",
       category: "Pants"
     },
     {
       id: 18, 
       name: "Cable Knit Sweater",
       variants: '["charcoal", "warm white", "white", "light gold", "light blue"]',
-      description: "",
+      description: "Slightly loose fitting, softly cable-knitted sweater. Round neckline and long sleeves.",
       category: "Sweaters"
     },
     {
       id: 19, 
       name: "Wide Leg Joggers",
       variants: '["black", "graphite", "navy", "tan"]',
-      description: "",
+      description: "Loose-fit, wide leg sweatpants. Elasticized waist.",
       category: "Pants"
     },
     {
       id: 20, 
       name: "Loafers",
       variants: '["charcoal", "black", "dark brown"]',
-      description: "",
+      description: "Rounded-toe loafers with a tab on front. Satin Lining.",
       category: "Shoes"
     }
   ]);
